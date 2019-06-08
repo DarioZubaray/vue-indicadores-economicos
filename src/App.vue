a88843ba81150a49d035fcb10da5aa8c7f39d7b4
dariozubaray <dzubaray@gmail.com>
2019-06-08 14:17:07 -0300

    agregando la vista con un datepicker, cards de vuetify, consumiendo una api con axios, utilizando vuex para cargar el loading

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
-    </v-toolbar>
-
     <v-content>
-      <router-view/>
+
+      <v-container>
+        <router-view/>
+        <v-dialog
+          v-model="loading.estado"
+          hide-overlay
+          persistent
+          width="300"
+        >
+          <v-card
+            :color="loading.color"
+            dark
+          >
+            <v-card-text>
+              {{loading.titulo}}
+              <v-progress-linear
+                indeterminate
+                color="white"
+                class="mb-0"
+              ></v-progress-linear>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+       </v-container>
+
     </v-content>
   </v-app>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 
 export default {
   name: 'App',
@@ -30,6 +39,9 @@ export default {
     return {
       //
     }
+  },
+  computed: {
+    ...mapState(['loading'])
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,62 @@
 <template>
-  <HelloWorld />
+  <v-layout :wrap="true">
+    <v-flex xs12>
+      <v-card>
+         <v-date-picker 
+            v-model="fecha" 
+            full-width 
+            locale="es-es" 
+            color="info"
+            :min="minimo"
+            :max="maximo"
+            @change="getDolar(fecha)"
+          ></v-date-picker>
+      </v-card>
+      <v-card color="secondary" dark>
+        <v-card-text class="display-1 text-xs-center">
+          {{dolar}}
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
+  import axios from 'axios'
+  import {mapMutations} from 'vuex'
   export default {
-    components: {
-      HelloWorld
+    data() {
+      return {
+        fecha: new Date().toISOString().substr(0, 10),
+        minimo: '1984-01-01s',
+        maximo: new Date().toISOString().substr(0, 10),
+        dolar: ''
+      }
+    },
+    methods: {
+      ...mapMutations(['mostrarLoading', 'ocultarLoading']),
+      async getDolar(dia) {
+        let arrayFecha = dia.split(['-'])
+        let ddmmyyyy = arrayFecha[2]+"-"+arrayFecha[1]+"-"+arrayFecha[0]
+        try {
+          this.mostrarLoading({titulo: 'Buscando información', color: 'secondary'})
+          const url = `https://mindicador.cl/api/dolar/${ddmmyyyy}`
+          let datos = await axios.get(url)
+          if(datos.data.serie.length > 0){
+            this.dolar = "$" + await datos.data.serie[0].valor
+          } else {
+            this.dolar = "Sin datos"
+          }
+        } catch(e) {
+          console.error(e)
+        } finally {
+          this.ocultarLoading()
+        }
+        
+      }
+    },
+    created() {
+      this.getDolar(this.fecha)
     }
   }
 </script>
